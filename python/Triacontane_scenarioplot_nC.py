bf049eb6e5e8a78b544ec40a05f7aa3b710315eb
Aaron Wiegel <aawiegel@gmail.com>
2017-01-16 13:57:27 -0800

    Update Triacontane_scenarioplot_nC.py

--- python/Triacontane_scenarioplot_nC.py
+++ python/Triacontane_scenarioplot_nC.py
@@ -16,7 +16,7 @@ import matplotlib.cm as cm
 
 # Define file name dictionary for looping
 
-ScenarioFiles = {'Scenario 1' : "tri_109nm_sc1.xml", 'Scenario 1A' : "tri_109nm_sc1a.xml", 'Scenario 2' : 'tri_109nm_sc2.xml', 'Scenario 1A UL' : 'tri_10xevap_109nm_sc1a.xml', 'Scenario 1A LL' : 'tri_tenthevap_109nm_sc1a.xml', 'Scenario 1 atm' : 'tri_109nm_atm_sc1.xml', 'Scenario 1A atm' : 'tri_109nm_atm_sc1a.xml'}
+ScenarioFiles = {'Scenario 1' : "tri_109nm_sc1.xml", 'Scenario 2' : 'tri_109nm_sc2.xml'}
 
 # Define scenario dictionary
 
@@ -27,8 +27,6 @@ Scenarios = {}
 carbon_min, carbon_max = 1, 30
 
 missing_carbon_no = [1, 30]
-
-#carbon_list = [["C30", 30], ["C30_COOH", 30], ["C30_COOH_O", 30], ["C30_HOOCCOOH", 30], ["C30_O2", 30], ["C30_O", 30]]
 
 carbon_no_dict = {"nC30" : [["C30", 1], ["C30_COOH", 1], ["C30_COOH_O", 1], ["C30_HOOCCOOH", 1], ["C30_O2", 1], ["C30_O", 1]]}
 
@@ -46,16 +44,6 @@ for i in range(carbon_min, carbon_max+1, 1):
         
         carbon_list.append(["n"+no_C, i])        
         
-        #carbon_list.append(["C"+str(i), i])
-        #carbon_list.append(["C"+str(i)+"_OC", i])
-        #carbon_list.append(["C"+str(i)+"_OH", i])
-        #carbon_list.append(["C"+str(i)+"_O2", i])
-        #carbon_list.append(["C"+str(i)+"_COOH_O", i])
-        #carbon_list.append(["C"+str(i)+"_COOH", i])
-        #carbon_list.append(["C"+str(i)+"_HOOCCOOH", i])
-        #carbon_list.append(["C"+str(i)+"_R", i])
-        #carbon_list.append(["C"+str(i)+"_COOOH", i])
-
 oxygen_list = [["OC_sec", 1], ["OCH_prim", 1], ["OHCH2_prim", 1], ["OHCH_sec", 1], ["OC_alpha", 1], ["OHCH_alpha", 1], ["HO_OOC_prim", 3], ["HOOCH2_prim", 2], ["HOOCH_alpha", 2], ["HOOCH_sec", 2], ["HOOC_prim", 2]]
 
 oxygen_list_2diff = [["OC_sec", 1], ["OCH_prim", 1], ["OHCH2_prim", 1], ["OHCH_sec", 1], ["OC_alpha", 1], ["OHCH_alpha", 1], ["HO_OOC_prim", 3], ["HOOCH2_prim", 2], ["HOOCH_alpha", 2], ["HOOCH_sec", 2], ["HOOC_prim", 2], ["OC_sec_f", 1], ["OCH_prim_f", 1], ["OHCH2_prim_f", 1], ["OHCH_sec_f", 1], ["HOOC_prim_f", 2], ["HO_OOC_prim_f", 3], ["HOOCH2_prim_f", 2], ["HOOCH_sec_f", 2]]
@@ -111,28 +99,3 @@ for scenario in ScenarioFiles.keys():
     Scenarios[scenario].calcContourInterpolated("O/C ratio", reverse_axis = True)
     Scenarios[scenario].calcContourInterpolated("H/C ratio", reverse_axis = True)
     
-
-# Utility function
-        
-#def getDepthProfile(scenario, position, contour, time_slice, depth_slice=5E-7, reverse_axis=True):
-#    """ Generates sum of a species 'data' at a certain depth profile 
-#        from a contour map, time_slice, and depth_slice.
-#        ContourMap of data must be generated first.
-#    """
-#    
-#    # Find closest index to time slice        
-#    time_i = (np.abs(scenario.time-time_slice)).argmin()
-#        
-#    # Find position of top compartment
-#    if(reverse_axis):
-#        max_position = scenario.compartments[(0, scenario.num_rows-1, 0)].positions["y"][time_i]
-#    else:
-#        max_position = scenario.compartments[(0, 0, 0)].positions["y"][time_i]
-#         
-#    min_position = max_position - depth_slice
-#    
-#    # Find closest index to min and max position
-#    max_pos_i = (np.abs(position - max_position)).argmin()
-#    min_pos_i = (np.abs(position - min_position)).argmin()
-#    
-#   return np.sum(contour[min_pos_i:max_pos_i])
